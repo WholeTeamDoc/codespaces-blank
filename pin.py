c90f18e8106846a59872d5ae6a6a571e65811943
TheAmazingDoc <150753116+WholeTeamDoc@users.noreply.github.com>
2023-12-05 12:29:31 -0500

    Update pin.py
    # figuring the PIN code input out

---
+++ pin.py
@@ -0,0 +1,10 @@
+# write code below 
+
+print('BANK OF DOC')
+
+    pin = int(input('Enter your PIN: '))
+    
+while pin != 1234:
+    pin = int(input('Enter your PIN again: '))
+if pin = 1234:
+   print('PIN accepted!')
