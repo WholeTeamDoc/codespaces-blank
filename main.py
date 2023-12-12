5f5df0402519b8b6ccf1f9d3c3f2a7e93c3eeb12
TheAmazingDoc <150753116+WholeTeamDoc@users.noreply.github.com>
2023-12-12 11:37:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("Hello world with your lame ass!!!!!")
-print('I am Gambit')
+# hey there is nothing to see here 
+
+#print("Hello World. Let's learn to code Python.")
+#print('I am the Gambit.')
+#print('I thought that was pretty funny to write.")
+#print("It was a joke. My name's Doc. 😂")
+
+# we are practicing python because we find it fun.
+
+print("I am Iron Man. \n" + "No, I am Tony Stark. \n" + "No, I am Papi.")
