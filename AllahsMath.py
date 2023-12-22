d513ae8194bc322479ec026acfcb2ca5bc34e5c3
TheAmazingDoc <150753116+WholeTeamDoc@users.noreply.github.com>
2023-12-22 12:04:46 -0500

    Update AllahsMath.py

---
+++ AllahsMath.py
@@ -0,0 +1,26 @@
+# Allahs Mathematics 👨🏾‍🔬
+
+import random
+
+num = random.randint(0, 9)
+
+if num == 0:
+    print("Cipher")
+if num == 1:
+    print("Knowledge")
+if num == 2:
+    print("Wisdom")
+if num == 3:
+    print("understanding")
+if num == 4:
+    print("culture/freedom")
+if num == 5:
+    print("power/refinement")
+if num == 6:
+    print("Equality")
+if num == 7:
+    print("GOD")
+if num == 8:
+    print("Build/Destroy")
+else:
+    print("Born")
