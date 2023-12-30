e357fb3fa209386c473740c15dba2a38c3824574
TheAmazingDoc <150753116+WholeTeamDoc@users.noreply.github.com>
2023-12-29 22:26:32 -0500

    Update Seasons.py
    Practicing

---
+++ Seasons.py
@@ -0,0 +1,23 @@
+#Codedex Lets Code the Seasons 🌱😎🍂❄️
+
+#Kapoof 🪄
+
+spring = [1, 2, 3]
+summer = [4, 5, 6]
+autumn = [7, 8, 9]
+winter = [10, 11, 12]
+
+month = int(input"Please kind user enter a month number (1-12): "))
+if month in [1, 2, 3]:
+ print('Spring 🌱')
+elif month in [4, 5, 6]:
+ print('Summer 😎')
+if month in [7, 8, 9]:
+ print('Autumn 🍂')
+elif month in [10, 11, 12]: 
+ print(' Winter ❄️')
+else:
+ print("Sorry user, that response is invalid. Please try again.")
+
+
+ 
