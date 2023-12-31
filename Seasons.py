924c35224ef87fe6ef2ae24dcaf129c59625b935
TheAmazingDoc <150753116+WholeTeamDoc@users.noreply.github.com>
2023-12-31 00:36:55 -0500

    Update Seasons.py
    Just a code prompt for the seasons

---
+++ Seasons.py
@@ -0,0 +1,19 @@
+#write code below 💖
+
+Winter = [1, 2, 3]:
+Spring = [4, 5, 6]:
+Summer = [7, 8, 9]:
+Autumn = [10, 11, 12]:
+
+month = int(input("Please enter a month number (1-12): "))
+
+if month in [1, 2, 3]: 
+ print('Winter ❄️')
+elif month in [4, 5, 6]:
+ print('Spring 🌱')
+if month in [7, 8, 9]:
+ print('Summer 😎')
+if month in [10, 11, 12]:
+ print('Autumn 🍂')
+else:
+ print ("Invalid")
